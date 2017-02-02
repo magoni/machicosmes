e440ea90a8ceca4b9482e058e032574fd55c333b
Evan Magoni <esm367@gmail.com>
2017-02-02 11:15:03 -0500

    link to pages with [[pagename]]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,16 @@ class ApplicationController < ActionController::Base
   helper_method :parse_links
 
   def parse_links(text)
+    # replace '[[pagetitle]]' with '<a href="pageurl">pagetitle</a>'
     exp = /(\[\[)(\w*?)(\]\])/
-    text = text.gsub(exp) { '<a href="' + fragment_path(Fragment.find_by(title: Regexp.last_match[2])) + '">'+ $2 +'</a>' }
+    text = text.gsub(exp) do
+      found_fragment = Fragment.find_by(title: Regexp.last_match[2])
+      if found_fragment then
+        '<a href="' + fragment_path(found_fragment) + '">'+ $2 +'</a>'
+      else
+        $2
+      end
+    end
     return text
   end
 end
